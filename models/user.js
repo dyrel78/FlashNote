5d54472b4f463d5bb602fc905e7227437a3ee89c
lumdy690 <lumdy690@student.otago.ac.nz>
2024-08-21 21:46:25 +1200

    Added bcrypt library and Hash + Salt function to models. #6

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   first_name: { type: String, required: true },
@@ -7,6 +8,27 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   username: { type: String, required: true, unique: true },
 });
+
+userSchema.pre("save", async function (next) {
+  try {
+    if (this.isModified("password")) {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 
 const User = mongoose.model("users", userSchema);
 
