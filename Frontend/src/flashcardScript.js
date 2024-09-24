14fee05528667b85be572cd0a73e5cecbf1f6966
lumdy690 <lumdy690@student.otago.ac.nz>
2024-09-25 02:31:14 +1200

    Flashcard Script that cleans output text and parses to suitable output

---
+++ Frontend/src/flashcardScript.js
@@ -0,0 +1,24 @@
+const FormatFlashcards = (text) => {
+    const parts= text.split('|');
+    const flashcards = [];
+    // let formattedText = '';
+
+
+    for (let i = 0; i < parts.length; i+=2) {
+        const question = parts[i];
+        const answer = parts[i+1];
+        flashcards.push({ question, answer });
+        // formattedText += `Q: ${question}\nA: ${answer}\n\n`;
+       
+
+
+
+
+    }
+    // return flashcards.join('');
+    return flashcards;
+    // return { flashcards, formattedText: formattedText.trim() };
+
+};
+
+export default FormatFlashcards;
