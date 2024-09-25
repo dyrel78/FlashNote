8459495b66017bf30e5c7b072697d7c443e6e4f6
lumdy690 <lumdy690@student.otago.ac.nz>
2024-09-25 13:45:51 +1200

    Re editing FlashCard model to ush to Notes Table

--- backend/models/flashcards.js
+++ backend/models/flashcards.js
@@ -4,11 +4,12 @@ const flashcardSchema = new mongoose.Schema({
   folder: { type: String, required: true },
   question: { type: String, required: true },
   answer: { type: String, required: true },
-  flashcard_name: { type: String, required: true },
+  note_name: { type: String, required: true },
+  format: { type: String, required: true },
   status: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
 });
 
-const Flashcard = mongoose.model("flashcards", flashcardSchema);
+const Flashcard = mongoose.model("notes", flashcardSchema);
 
 export default Flashcard;
