56ed1b615bfd40191abc035348574d80bc9c2f93
Zara <naeza379@student.otago.ac.nz>
2024-09-26 19:12:36 +1200

    changed notes folder to flash cards on Navbar

--- Frontend/src/components/Navbar.vue
+++ Frontend/src/components/Navbar.vue
@@ -2,7 +2,7 @@
     <nav class="flashnote-navbar">
       <ul>
         <li><a href="home">Home</a></li>
-        <li><a href="#">Notes Folders</a></li>
+        <li><a href="#">Flashcards</a></li>
         <li><a href="/view-notes-preview">Notes</a></li>
         <li><a href="/profile">Profile</a></li>
         <li><a href="/create-account">Create Account</a></li>
