fae32ebe6e4c317969f22564e1d6c60107fe5b80
lumdy690 <lumdy690@student.otago.ac.nz>
2024-09-28 00:48:34 +1200

    Docker containers now work

--- Docker.md
+++ Docker.md
@@ -23,3 +23,26 @@ https://cli.vuejs.org/guide/deployment.html#amazon-s3
 
     "baseUrl": "./",
     "baseUrl": "",
+
+
+# errors
+
+Error: ENOENT: no such file or directory, stat '/usr/src/frontend/dist/index.html'
+
+
+"scripts": {
+    // "serve": "vue-cli-service serve",
+    "serve": "serve -s dist",
+    "build": "vue-cli-service build",
+    "lint": "vue-cli-service lint",
+    "build:serve": "vue-cli-service build && vue-cli-service serve"
+  },
+
+
+# What i did
+
+Set a health check in docker compose so backend waits for frontend to be healthy and built
+
+Changed filed path in backend to work with docker ( the dist files lol in res.sendFile)
+
+  
--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use(cors());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, 'frontend','dist')));
 
 
 app.use(
@@ -37,7 +38,17 @@ app.use("/api/llm", llmRoutes);
 app.use("/api/pdf", pdfRoutes);
 app.get("*", (req, res) => {
   // res.sendFile(path.join(__dirname, "Frontend","dist", "index.html"));
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  try {
+    // Works for docker
+    res.sendFile(path.join(__dirname, 'frontend','dist', 'index.html'));
+
+  } catch (error) {
+    console.log(error);
+    // Works for local
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+
+  }
+  // res.sendFile("/usr/src/frontend/dist/index.html");
 
 });
 //Middleware to parse JSON bodies
