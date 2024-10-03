5f37398effc37f7acfdb71a0fb5649db13e994e2
lumdy690 <lumdy690@student.otago.ac.nz>
2024-10-03 21:48:38 +1300

    Bug fixed the nav bar - home button was not working for other pages with an extra slug

--- Frontend/src/components/Navbar.vue
+++ Frontend/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="flashnote-navbar">
       <ul>
-        <li><a href="home">Home</a></li>
+        <li><a href="/home">Home</a></li>
         <li><a href="#">Flashcards</a></li>
         <li><a href="/profile">Profile</a></li>
         <li><a href="/create-account">Create Account</a></li>
